b19ed668e7b6ff3e1a34b1eb600e20a47486851f
Mark Shteller <shlukiman@gmail.com>
2018-11-06 17:02:58 -0800

    Adding fog to the scene

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,8 +13,8 @@ public class CameraMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-        float oSize = Camera.main.orthographicSize;
+        //float oSize = Camera.main.orthographicSize;
         //transform.localPosition = new Vector3(target.localPosition.x - oSize, transform.localPosition.y, target.localPosition.z - oSize);
-        transform.localPosition = new Vector3(target.localPosition.x, transform.localPosition.y, target.localPosition.z -1.5f * oSize);
+        transform.localPosition = new Vector3(target.localPosition.x, transform.localPosition.y, target.localPosition.z -15);
 	}
 }
